2686f68f56ec6f6f84c6da3833597b62cca82ca4
Jacob Sanford <jsanford@unb.ca>
2020-02-26 12:39:31 -0400

    do not backup sys db

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -2,7 +2,7 @@
 MYSQL=/usr/bin/mysql
 MYSQLDUMP=/usr/bin/mysqldump
 
-DATABASES=`$MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+DATABASES=`$MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys)"`
 
 for DB in $DATABASES; do
   $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB | gzip > "$DB.gz"
