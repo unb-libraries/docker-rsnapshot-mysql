35243f2cd3b60e88a5bf3f45c08cf618d39fa276
Jacob Sanford <jsanford@unb.ca>
2020-02-27 08:41:59 -0400

    Use upstream mysql-backup.sh script

--- scripts/mysql-backup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-MYSQL=/usr/bin/mysql
-MYSQLDUMP=/usr/bin/mysqldump
-
-DATABASES=`$MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys)"`
-
-for DB in $DATABASES; do
-  $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB | gzip > "$DB.gz"
-done
