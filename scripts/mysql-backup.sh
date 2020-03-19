340a21e0750d54ee45a638e5af3f5c28dcfd13ad
Jacob Sanford <jsanford@unb.ca>
2020-03-19 19:40:46 -0300

    IN-212 Improve whitespace/readability

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -5,16 +5,16 @@ ROWS_PER_FILE='500000'
 TMP_DIR=$(mktemp -d -t sqldumps-XXXXXXXXXX)
 
 $MYDUMPER \
---host=MYSQL_HOSTNAME \
---port=MYSQL_PORT \
---user=MYSQL_USER_NAME \
---password=MYSQL_USER_PASSWORD \
---regex="^(Database|information_schema|performance_schema|sys|mysql)$" \
---outputdir=$TMP_DIR \
---rows=100000 \
---compress \
---build-empty-files \
---compress-protocol
+  --host=MYSQL_HOSTNAME \
+  --port=MYSQL_PORT \
+  --user=MYSQL_USER_NAME \
+  --password=MYSQL_USER_PASSWORD \
+  --regex="^(Database|information_schema|performance_schema|sys|mysql)$" \
+  --outputdir=$TMP_DIR \
+  --rows=100000 \
+  --compress \
+  --build-empty-files \
+  --compress-protocol
 
 mv "$TMP_DIR/*" .
 rm -rf $TMP_DIR
