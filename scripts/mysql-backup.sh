d34d11bd6fcfcb33872898a6e226673e6c77c02b
Jacob Sanford <jsanford@unb.ca>
2020-03-19 19:41:14 -0300

    IN-212 Exclude listed, not include

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -9,7 +9,7 @@ $MYDUMPER \
   --port=MYSQL_PORT \
   --user=MYSQL_USER_NAME \
   --password=MYSQL_USER_PASSWORD \
-  --regex="^(Database|information_schema|performance_schema|sys|mysql)$" \
+  --regex="^(?!(Database|information_schema|performance_schema|sys|mysql))$" \
   --outputdir=$TMP_DIR \
   --rows=100000 \
   --compress \
