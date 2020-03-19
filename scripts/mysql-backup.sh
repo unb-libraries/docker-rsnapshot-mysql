712262205d84acd3e0d5e350399cca692aa5e535
Jacob Sanford <jsanford@unb.ca>
2020-03-19 16:36:19 -0300

    NBNP-273 Do not gzip when dumping database

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -5,6 +5,7 @@ MYSQLDUMP=/usr/bin/mysqldump
 DATABASES=`$MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "^(Database|information_schema|performance_schema|sys)$"`
 
 for DB in $DATABASES; do
-  $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB | gzip > "/tmp/$DB.gz"
+  $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB > "/tmp/$DB"
+  gzip -9 "/tmp/$DB"
   mv "/tmp/$DB.gz" "./$DB.gz"
 done
