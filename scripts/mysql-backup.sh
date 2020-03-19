6d6768a380cdefe3e5ceb69a4bc75ee746965d08
Jacob Sanford <jsanford@unb.ca>
2020-03-19 16:12:37 -0300

    Dump to on-disk device, then move to NFS

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -5,5 +5,6 @@ MYSQLDUMP=/usr/bin/mysqldump
 DATABASES=`$MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "^(Database|information_schema|performance_schema|sys)$"`
 
 for DB in $DATABASES; do
-  $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB | gzip > "$DB.gz"
+  $MYSQLDUMP -h MYSQL_HOSTNAME -P MYSQL_PORT --force --events --ignore-table=mysql.event --opt --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD --databases $DB | gzip > "/tmp/$DB.gz"
+  mv "/tmp/$DB.gz" "./$DB.gz"
 done
