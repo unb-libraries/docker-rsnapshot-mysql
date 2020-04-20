c1e212b864181e124f926b663702623bc8ca7bfe
Jacob Sanford <jsanford@unb.ca>
2020-04-20 11:45:38 -0300

    IN-212 Re-add populating env vars at startup time

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -4,16 +4,16 @@ MYSQL=/usr/bin/mysql
 ROWS_PER_FILE='100000'
 
 TMP_DIR=$(mktemp -d -t sqldumps-XXXXXXXXXX)
-DATABASES=$($MYSQL -h $MYSQL_HOSTNAME -P $MYSQL_PORT --user=$MYSQL_USER_NAME -p$MYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "^(Database|information_schema|performance_schema|sys)$")
+DATABASES=$($MYSQL -h MYSQL_HOSTNAME -P MYSQL_PORT --user=MYSQL_USER_NAME -pMYSQL_USER_PASSWORD -e "SHOW DATABASES;" | grep -Ev "^(Database|information_schema|performance_schema|sys)$")
 
 for DB in $DATABASES; do
   OUTPUT_DIR="$TMP_DIR/$DB"
   mkdir "$OUTPUT_DIR"
   $MYDUMPER \
-    --host=$MYSQL_HOSTNAME \
-    --port=$MYSQL_PORT \
-    --user=$MYSQL_USER_NAME \
-    --password=$MYSQL_USER_PASSWORD \
+    --host=MYSQL_HOSTNAME \
+    --port=MYSQL_PORT \
+    --user=MYSQL_USER_NAME \
+    --password=MYSQL_USER_PASSWORD \
     --database=$DB \
     --outputdir=$OUTPUT_DIR \
     --rows=$ROWS_PER_FILE \
